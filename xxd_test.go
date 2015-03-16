a2c9fbd6ce93c29e75cba80e2309aa886ff72268
Eric <ericscottlagergren@gmail.com>
2015-03-16 16:22:12 -0700

    added tests and fixed formatting

--- xxd_test.go
+++ xxd_test.go
@@ -1,90 +1,33 @@
 package main
 
 import (
-	"bytes"
-	"crypto/rand"
-	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os/exec"
-	"strings"
 	"testing"
-	"testing/quick"
 )
 
-var xxdFile = flag.String("xxdFile", "", "File to test against.")
-
-func TestXXD(t *testing.T) {
-	if *xxdFile == "" {
-		t.Skip("-xxdFile argument not given")
-	}
-	data, err := ioutil.ReadFile(*xxdFile)
+func TestOutFile(t *testing.T) {
+	f, err := ioutil.ReadFile("outfile")
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
-	test := func(fn func(r io.Reader, w io.Writer, s string) error) func(n uint64) []string {
-		return func(n uint64) []string {
-			size := n % uint64(len(data))
-			fmt.Printf("%d\n", size)
-			var out bytes.Buffer
-			if err := fn(&pathologicalReader{data[0:size]}, &out, ""); err != nil {
-				return []string{err.Error()}
-			}
-			return strings.Split(out.String(), "\n")
+
+	cmd := exec.Command("./xxd", "image.jpg")
+	b, err := cmd.Output()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != string(f) {
+		t.FailNow()
+	}
+
+	for i, v := range f {
+		if b[i] != v {
+			fmt.Printf("Expected:\n%c\n\nGot:\n%c\n%d", v, b[i], i)
+			t.FailNow()
 		}
 	}
-	if err := quick.CheckEqual(test(XXD), test(xxdNative), nil); err != nil {
-		cErr := err.(*quick.CheckEqualError)
-		size := cErr.In[0].(uint64) % uint64(len(data))
-		for i := range cErr.Out1[0].([]string) {
-			got := cErr.Out1[0].([]string)[i]
-			want := cErr.Out2[0].([]string)[i]
-			if got != want {
-				t.Errorf("size: %d\n\ngot : %s\nwant: %s\n", size, got, want)
-				break
-			}
-		}
-	}
-}
 
-type pathologicalReader struct {
-	data []byte
-}
-
-func (p *pathologicalReader) Read(b []byte) (int, error) {
-	n := len(b)
-	if n > len(p.data) {
-		n = len(p.data)
-	}
-	if n > 1 {
-		n--
-	}
-	copy(b, p.data[0:n])
-	p.data = p.data[n:]
-	if len(p.data) == 0 {
-		return n, io.EOF
-	}
-	return n, nil
-}
-
-func BenchmarkXXD(b *testing.B) {
-	b.StopTimer()
-	data := make([]byte, b.N)
-	if _, err := io.ReadFull(rand.Reader, data); err != nil {
-		b.Fatal(err)
-	}
-	buf := bytes.NewBuffer(data)
-	b.StartTimer()
-	if err := XXD(buf, ioutil.Discard, ""); err != nil {
-		b.Fatal(err)
-	}
-}
-
-func xxdNative(r io.Reader, w io.Writer, s string) error {
-	xxd := exec.Command("xxd", "-")
-	xxd.Stdin = r
-	xxd.Stdout = w
-	xxd.Stderr = w
-	return xxd.Run()
 }
